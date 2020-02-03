ba3e13e64b3d6776ae6d0c985830a2805b104a7c
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-02-03 15:46:08 -0500

    Rewrote toggleActive function

--- JS/script.js
+++ JS/script.js
@@ -4,8 +4,11 @@ function toggleOpen() {
     this.classList.toggle('open');
 }
 
-function toggleActive() {
-    this.classList.toggle('open-active');
+function toggleActive(e) {
+    console.log(e.propertyName);
+    if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active');
+    }
 }
 
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
